2b38b8987d6c341e9c38712d80fcf45cc35b5fdc
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-12-21 16:10:45 +0900

    Create Bullet Script

---
+++ Source/Shooter/Private/Bullet.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Bullet.h"
+
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+ABullet::ABullet()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+#pragma region Initialize Colliders Meshes
+	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collision"));			//Create box collider component
+	SetRootComponent(boxComp);														//Set as root		
+	boxComp->SetBoxExtent(FVector(50));												//change box size
+	boxComp->SetWorldScale3D(FVector(0.75f, 0.25f, 1.00f));
+
+
+	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));	//crate SN component
+	meshComp->SetupAttachment(RootComponent);										//Child the mesh comp
+	meshComp->SetRelativeLocation(FVector(0,0,-50));			//move box collider to match position with mesh
+
+
+#pragma endregion
+}
+
+// Called when the game starts or when spawned
+void ABullet::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABullet::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	myDirection = GetActorForwardVector();
+
+	SetActorLocation(GetActorLocation() + myDirection * moveSpeed * DeltaTime);
+}
+
+
