08a29eb27c8026697e642220c925c03eed7be748
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-12-23 14:45:01 +0900

    Update Enemy.cpp
    -Fix Error in TActorIterator<APlayerMove
    -Set Collision Channel

--- Source/Shooter/Private/Enemy.cpp
+++ Source/Shooter/Private/Enemy.cpp
@@ -4,6 +4,9 @@
 #include "Enemy.h"
 #include "Components/BoxComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "PlayerMove.h"
+#include "EngineUtils.h"
 
 
 // Sets default values
@@ -11,13 +14,29 @@ AEnemy::AEnemy()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+#pragma region Initialize Colliders Meshes
 	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("Collision"));
 	SetRootComponent(boxComp);
 	boxComp->SetBoxExtent(FVector(50)); 
 
+#pragma region Collision
+	//enable collision with query and physics mode
+	boxComp->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+
+	//set object type's reply channel to 
+	boxComp->SetCollisionObjectType(ECC_GameTraceChannel12);		//ECC = ECollision Channel
+	
+	//Set all to ignore
+	boxComp->SetCollisionResponseToAllChannels(ECR_Ignore);			//ECR = ECollision Response
+
+	//Set player and bullet to overlap
+	boxComp->SetCollisionResponseToChannel(ECC_GameTraceChannel1, ECR_Overlap);	//Player
+	boxComp->SetCollisionResponseToChannel(ECC_GameTraceChannel3, ECR_Overlap);	//Bullet
+#pragma endregion
+
 	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
 	meshComp->SetupAttachment(RootComponent);
+#pragma endregion 
 
 }
 
@@ -26,6 +45,28 @@ void AEnemy::BeginPlay()
 {
 	Super::BeginPlay();
 	
+#pragma region Enemy Direction
+	float randomNum = FMath::RandRange(0.1f, 1.0f);       //get random number
+
+	if (randomNum <= followProb) {
+		//method 1 of finding player
+		//AActor* target = UGameplayStatics::GetActorOfClass(GetWorld(), APlayerMove::StaticClass());
+
+		//method 2 of finding player
+		//APlayerMove* target;
+		for (TActorIterator<APlayerMove> it(GetWorld()); it; ++it) {
+			target = *it;
+		}
+	
+		FVector targetDir = target->GetActorLocation() - GetActorLocation();
+		targetDir.Normalize();					//need to normalize or else it will shoot out very far far
+		
+		direction = targetDir;
+	}
+	else {
+		direction = GetActorForwardVector();
+	}
+#pragma endregion
 }
 
 // Called every frame
@@ -33,8 +74,8 @@ void AEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	direction = GetActorForwardVector();
-
+#pragma region Enemy Move
 	SetActorLocation(GetActorLocation() + direction * moveSpeed * DeltaTime);
+#pragma endregion 
 }
 
