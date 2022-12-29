98a27704672b83753673a5976c1ce65ab2992146
mysk423 <57009810+yoponchik@users.noreply.github.com>
2022-12-29 11:57:15 +0900

    Create MainWidget Scripts
    -Changing textblocks

---
+++ Source/Shooter/Private/MainWidget.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainWidget.h"
+#include "MyGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/TextBlock.h"
+
+
+void UMainWidget::PrintCurrentScore()
+{
+	AMyGameModeBase* myGM = Cast<AMyGameModeBase>(UGameplayStatics::GetGameMode(this));
+
+	if (myGM != nullptr) {
+		//change int type to SetText 
+		FText scoreText = FText::AsNumber(myGM->GetCurrentScore());
+
+		//change uIcurrrentScore's text
+		uICurrentScore->SetText(scoreText);
+	}
+}
---
+++ Source/Shooter/Public/MainWidget.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API UMainWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI Settings|Score", meta = (BindWidget));
+	class UTextBlock* uICurrentScore;
+
+	void PrintCurrentScore();
+
+};
